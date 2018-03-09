f6bc20de6f1666dbd80b232c21ab1f841133ff5d
Connor Jessup <cjessup@umich.edu>
2018-03-09 17:02:25 -0500

    Basic requirements and server running

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('hello wikistacks');
+'use strict';
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment 
+// instance, which we'll want to use to add Markdown support later.
+const env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
+
+// logging middleware
+app.use(morgan('dev'));
+
+app.get('/',function(req, res){
+res.json("hghdhgfhgf");
+
+})
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+const server = app.listen(3000, function(){
+    console.log('listening on port 3000');
+});
